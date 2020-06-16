9f17ad0393db1df78a8a2def75febd2b389cc3e4
CodeOfDutyDev <webwalterwhrite@gmail.com>
2020-06-16 13:44:34 +0200

    :speech_balloon: remove default text react

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title page', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/TWILIO-SMS-VERIFY/i);
+  expect(titleElement).toBeInTheDocument();
 });
